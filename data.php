1277675cdb19eea9a57ec12954babeadfcde8def
aman <amanrajbhar000@gmail.com>
2024-12-26 16:26:13 +0530

    first commit

---
+++ data.php
@@ -0,0 +1,13 @@
+<?php
+$title=$_POST['title'];
+// echo "Name Is".$name ."Phone Number:".$phone;
+include 'database.php';
+$sql="INSERT INTO todo(title)VALUES('$title')";
+$result=mysqli_query($conn, $sql);
+if($result){
+    header("location: ./index.php");
+}
+else{
+    // echo "Sorry We Can't Connect";
+}
+?>
---
+++ editaction.php
@@ -0,0 +1,11 @@
+<?php
+       $id=$_POST['id'];
+       $title=$_POST['title'];
+       
+    include 'database.php';
+    $sql="UPDATE todo SET title='$title' WHERE id=$id";
+    $result=mysqli_query($conn, $sql);
+    if($result){
+        header("location: ./index.php");
+    }
+?>
